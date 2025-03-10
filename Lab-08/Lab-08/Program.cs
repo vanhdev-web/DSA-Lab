925f74a00ab65808d1f9f8a746742ef0bb16e91e
Vietanh <dovietanh240405@gmail.com>
2025-03-10 14:43:32 +0700

    Lab-08

---
+++ Lab-08/Lab-08/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab_08
+{
+    public delegate void WithdrawalHandler(string s);
+    public delegate void TransferHandler(string s);
+    public delegate void NotEnoughBalance(string s);
+    public class Account
+    {
+        public int TotalMoney;
+        public Account(int totalMoney)
+        {
+            this.TotalMoney = totalMoney;
+        }
+
+        public override string ToString()
+        {
+            return $"Số tiền trong tài khoản là : {this.TotalMoney}";
+        }
+
+        public event WithdrawalHandler OnWithdrawalHandler;
+        public event TransferHandler OnTransferHandler;
+        public event NotEnoughBalance OnNotEnoughBalance;
+
+        public void Withdrawal(int money)
+        {
+            if (money > this.TotalMoney)
+            {
+                OnNotEnoughBalance("Số dư tài khoản không đủ");
+            }
+            else
+            {
+                this.TotalMoney -= money;   
+                if (OnWithdrawalHandler != null)
+                {
+                    OnWithdrawalHandler($"Đã rút {money} VND");
+                }
+            }
+        }
+
+        public void Transfer (int money)
+        {
+            if (money > this.TotalMoney)
+            {
+                OnNotEnoughBalance("Số dư tài khoản không đủ");
+            }
+            else
+            {
+                this.TotalMoney -= money;
+                if (OnTransferHandler != null)
+                {
+                    OnTransferHandler($"Đã chuyển {money} VND");
+                }
+            }
+        }
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            Account account = new Account(500000);
+            Console.WriteLine(account.ToString());
+            Console.WriteLine("______________________________");
+            account.OnWithdrawalHandler += Account_OnWithdrawalHandler;
+            account.OnTransferHandler += Account_OnTransferHandler;
+            account.OnNotEnoughBalance += Account_OnNotEnoughBalance;
+
+            account.Withdrawal(30000);
+            Console.WriteLine(account.ToString());
+            Console.WriteLine("______________________________");
+            account.Transfer(40000);
+            Console.WriteLine(account.ToString());
+            Console.WriteLine("______________________________");
+            account.Withdrawal(600000);
+            Console.WriteLine(account.ToString());
+            
+            Console.ReadKey();
+
+        }
+
+        private static void Account_OnNotEnoughBalance(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        private static void Account_OnTransferHandler(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        private static void Account_OnWithdrawalHandler(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+   
+
+    
+
+    
+}
---
+++ Lab-08/Lab-08/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab-08")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab-08")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a498821-4557-4bfd-a38b-8ffca786f51f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
